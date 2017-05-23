2dcb3ec0c41da0a836c13fda9c182ce8266bcc0f
Sergiu Glavatchi <sglavatchi@endava.net>
2017-05-23 09:55:25 +0300

    started hero serach component

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -33,7 +33,6 @@ export class HeroService {
     }
 
     getHero(id: number): Promise<Hero> {
-
         const url = `${this.heroesUrl}/${id}`;
 
         return this.http.get(url)
@@ -43,7 +42,6 @@ export class HeroService {
     }
 
     update(hero: Hero): Promise<Hero> {
-
         const url = `${this.heroesUrl}/${hero.id}`;
 
         return this.http
@@ -52,5 +50,24 @@ export class HeroService {
             .then(() => hero)
             .catch(this.handleError);
 
+    }
+
+    create(name: string): Promise<Hero> {
+
+        return this.http
+            .post(this.heroesUrl, JSON.stringify({ name: name }), { headers: this.headers })
+            .toPromise()
+            .then(res => res.json().data as Hero)
+            .catch(this.handleError);
+
+    }
+
+    delete(id: number): Promise<void> {
+        const url = `${this.heroesUrl}/${id}`;
+
+        return this.http.delete(url, { headers: this.headers })
+            .toPromise()
+            .then(() => null)
+            .catch(this.handleError);
     }
 }
